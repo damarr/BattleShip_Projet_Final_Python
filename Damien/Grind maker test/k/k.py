c4c2629e6c0d4e5adbf3949f093d0f29607c8057
Damien Arroyo <guitarepower@gmail.com>
2015-11-16 18:23:26 -0500

    Modification!
    Création d'une fonction de création de grid facile a implanter dans
    nimporte quel programme, facilement maléable, positionable. Permet de
    créer des grid de nimporte quelle grandeur facilement!
    Bien que la fonction prend beaucoup de paramètres, elle sauve du temps
    en étaut aussi spécifique.

--- Damien/Grind maker test/k/k.py
+++ Damien/Grind maker test/k/k.py
@@ -2,28 +2,30 @@
 import random
 import time
 display = turtle.Screen()
-display.setup(500,500)
+display.setup(800,800)
 backColor = 'White'
-gridColor = 'Black'
+gridColor = 'Blue'
 
 def woah():
     screen.bgcolor(random.uniform(0,1),random.uniform(0,1),random.uniform(0,1))
 
-def drawGrid(nbHeight,margin):
+def drawGrid(nbHeight,margin,windowWeight,posX,posY,screen):
     #Initiation of turtle
-    pixelPerSquare = int((display._window_size()[0] - 2*margin)/nbHeight)
-    compensation = int(display._window_size()[0]/2)
+    pixelPerSquare = (windowWeight - 2*margin)/nbHeight
+    compensation = windowWeight/2
     print(pixelPerSquare)
     basicTurtle = turtle.Turtle()
     basicTurtle._tracer(10,1000)
     basicTurtle.color(gridColor)
     basicTurtle.penup()
-    basicTurtle.setposition(-compensation + margin,compensation - margin)
+    basicTurtle.setposition(-screen._window_size()[0]/2 + posX + margin,screen._window_size()[0]/2 - posY - margin)
+    #basicTurtle.setposition(-compensation + margin,compensation - margin)
     basicTurtle.pendown()
+    
     
     #Drawing
     for x in range (4):
-        basicTurtle.forward(display._window_size()[0] - 2*margin)
+        basicTurtle.forward(windowWeight - 2*margin)
         basicTurtle.right(90)
 
     for x in range (0,nbHeight):
@@ -32,13 +34,15 @@ def drawGrid(nbHeight,margin):
                 basicTurtle.forward(pixelPerSquare)
                 basicTurtle.right(90)
             basicTurtle.forward(pixelPerSquare)
-        basicTurtle.backward(display._window_size()[0] - 2*margin)
+        basicTurtle.backward(windowWeight - 2*margin)
         basicTurtle.right(90)
         basicTurtle.forward(pixelPerSquare)
         basicTurtle.left(90)
 
 
-drawGrid(10,10)
+drawGrid(11,10,400,200,350,display)
+drawGrid(11,10,250,275,75,display)
+#drawGrid(10,10,500/2,500,500)
 """
 while True:
     screen.title(start_time)
