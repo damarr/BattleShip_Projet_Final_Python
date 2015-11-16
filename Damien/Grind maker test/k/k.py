bf99e3d54a8de122f2e77f1b1cc8012345b98d8c
Damfurrywolf <guitarepower@gmail.com>
2015-11-16 17:54:50 -0500

    Fabriqueur de grid de grandeur custom
    Surtout pour des tests mais peut être vas être utilisé au final

---
+++ Damien/Grind maker test/k/k.py
@@ -0,0 +1,48 @@
+﻿import turtle
+import random
+import time
+display = turtle.Screen()
+display.setup(500,500)
+backColor = 'White'
+gridColor = 'Black'
+
+def woah():
+    screen.bgcolor(random.uniform(0,1),random.uniform(0,1),random.uniform(0,1))
+
+def drawGrid(nbHeight,margin):
+    #Initiation of turtle
+    pixelPerSquare = int((display._window_size()[0] - 2*margin)/nbHeight)
+    compensation = int(display._window_size()[0]/2)
+    print(pixelPerSquare)
+    basicTurtle = turtle.Turtle()
+    basicTurtle._tracer(10,1000)
+    basicTurtle.color(gridColor)
+    basicTurtle.penup()
+    basicTurtle.setposition(-compensation + margin,compensation - margin)
+    basicTurtle.pendown()
+    
+    #Drawing
+    for x in range (4):
+        basicTurtle.forward(display._window_size()[0] - 2*margin)
+        basicTurtle.right(90)
+
+    for x in range (0,nbHeight):
+        for y in range (0,nbHeight):
+            for w in range (4):
+                basicTurtle.forward(pixelPerSquare)
+                basicTurtle.right(90)
+            basicTurtle.forward(pixelPerSquare)
+        basicTurtle.backward(display._window_size()[0] - 2*margin)
+        basicTurtle.right(90)
+        basicTurtle.forward(pixelPerSquare)
+        basicTurtle.left(90)
+
+
+drawGrid(10,10)
+"""
+while True:
+    screen.title(start_time)
+
+
+screen.onkey(woah,"w")
+"""
